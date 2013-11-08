c0059c601177da6d40fa45bfdc7b3b8132087d38
Gomis Florian <florian.gomis@viacesi.fr>
2013-11-08 11:28:06 +0100

    addition of player with 1 button Play/Stop, oneslider to trackmusic time and one label

--- WebMusicPlayer/homeControllerPlayer.h
+++ WebMusicPlayer/homeControllerPlayer.h
@@ -7,11 +7,21 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MediaPlayer/MediaPlayer.h>
+#import <AVFoundation/AVFoundation.h>
 
-@interface homeControllerPlayer : UIViewController
+@interface homeControllerPlayer : UIViewController 
 @property (strong, nonatomic) IBOutlet UIImageView *imageDetail;
 @property (strong, nonatomic) IBOutlet UILabel *titreDetail;
 @property (nonatomic) NSString *titreDetailString;
+@property (nonatomic) NSString *urlSong;
 @property (nonatomic) UIImage *imageDetailUimage;
+
+// Concern Player in view
+@property (strong, nonatomic) AVPlayer *audioPlayer;
+@property (strong, nonatomic) IBOutlet UIButton *togglePlayPause;
+@property (strong, nonatomic) IBOutlet UISlider *sliderOutlet;
+@property (strong, nonatomic) IBOutlet UILabel *durationOutlet;
+@property (nonatomic) AVPlayerItem * currentItem;
 
 @end
--- WebMusicPlayer/homeControllerPlayer.m
+++ WebMusicPlayer/homeControllerPlayer.m
@@ -16,14 +16,25 @@
 @synthesize titreDetail = _titreDetail;
 @synthesize titreDetailString = _titreDetailString;
 @synthesize imageDetailUimage = _imageDetailUimage;
+@synthesize audioPlayer = _audioPlayer;
+@synthesize urlSong = _urlSong;
+@synthesize sliderOutlet = _sliderOutlet;
+@synthesize durationOutlet = _durationOutlet;
+@synthesize currentItem = _currentItem;
 
 - (id)initWithCoder:(NSCoder *)aDecoder
 {
     if ((self = [super initWithCoder:aDecoder])){
         _imageDetail = [[UIImageView alloc] init];
-        _titreDetailString = @"Default Title Detail";
+        _titreDetailString = @"DefaultTitleDetail";
         _titreDetail = [[UILabel alloc] init];
         _imageDetailUimage = [[UIImage alloc] init];
+        _audioPlayer = [[AVPlayer alloc] init];
+        _sliderOutlet = [[UISlider alloc] init];
+        _durationOutlet = [[UILabel alloc] init];
+        _currentItem = [[AVPlayerItem alloc] init];
+        _urlSong = @"DefaultUrl";
+        _audioPlayer = [[AVPlayer alloc]init];
     }
     return self;
 }
@@ -33,12 +44,55 @@
     [super viewDidLoad];
     [self.imageDetail setImage:self.imageDetailUimage];
     self.titreDetail.text = self.titreDetailString;
+    
+    [self configurePlayer];
+    
+    // son qu'on va jouer
+    self.currentItem = [AVPlayerItem playerItemWithURL:[[NSURL alloc] initWithString:self.urlSong]];
+    [self.audioPlayer replaceCurrentItemWithPlayerItem:self.currentItem];
+    [self.sliderOutlet setMaximumValue:self.audioPlayer.currentItem.duration.value/self.audioPlayer.currentItem.duration.timescale];
+
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+- (IBAction)togglePlayPause:(id)sender {
+    if(self.togglePlayPause.selected) {
+        [self.audioPlayer pause];
+        [self.togglePlayPause setSelected:NO];
+    } else {
+        [self.audioPlayer play];
+        [self.togglePlayPause setSelected:YES];
+    }
+}
+- (IBAction)slidderDragged:(id)sender {
+    [self.audioPlayer seekToTime:CMTimeMakeWithSeconds((int)(self.sliderOutlet.value) , 1)];
+}
+
+-(void) configurePlayer {
+    __block homeControllerPlayer * weakSelf = self;
+    [self.audioPlayer addPeriodicTimeObserverForInterval:CMTimeMakeWithSeconds(1, 1)
+                                                   queue:NULL
+                                              usingBlock:^(CMTime time) {
+                                                  if(!time.value) {
+                                                      return;
+                                                  }
+                                                  
+                                                  int currentTime = (int)((weakSelf.audioPlayer.currentTime.value)/weakSelf.audioPlayer.currentTime.timescale);
+                                                  int currentMins = (int)(currentTime/60);
+                                                  int currentSec  = (int)(currentTime%60);
+                                                  
+                                                  NSString * durationLabel =
+                                                  [NSString stringWithFormat:@"%02d:%02d",currentMins,currentSec];
+                                                  weakSelf.durationOutlet.text = durationLabel;
+                                                  weakSelf.sliderOutlet.value = currentTime;
+                                              }];
+    
 }
 
 
